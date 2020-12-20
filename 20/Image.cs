c0ca660ee6c4656a651457729c3f1c00fc84cdc5
Andrew Douglas <potterboy67@outlook.com>
2020-12-20 22:54:18 +0000

    a move towards part2 but not there by a mile.  Working from one corner doesn't work.

--- 20/Image.cs
+++ 20/Image.cs
@@ -16,7 +16,7 @@ namespace _20 {
         string[] image;
         public int num { get; init; }
 
-        List<Image> vertices;
+        public List<Image> vertices;
         public int NumVertices { get => this.vertices.Count; }
 
         string top;
@@ -82,6 +82,38 @@ namespace _20 {
             return g;
         }
 
+        public Image finalleft = null;
+        public Image finalright = null;
+        public Image finalup = null;
+        public Image finaldown = null;
+        public int x = 0;
+        public int y = 0;
+
+        internal bool TopLeft()
+        {
+            //work out if we're top left corner.
+            if (NumVertices != 2) return false;
+            Image one = vertices[0];
+            Image two = vertices[1];
+
+            if (RotateToFit(bottom, one, x => x.top) && RotateToFit(right, two, x => x.left)) {
+                finaldown = one;
+                finalright = two;
+                one.finalup = this;
+                two.finalleft = this;
+                return true;
+            }
+            else if (RotateToFit(bottom, two, x => x.top) && RotateToFit(right, one, x => x.left)) {
+                finaldown = two;
+                finalright = one;
+                two.finalup = this;
+                one.finalleft = this;
+                return true;
+            }
+
+            return false;
+        }
+
         public string matchImageCondition(Image image) {
             string total = "";
             total += matchEdgeCondition(image.top, "T");
@@ -106,6 +138,141 @@ namespace _20 {
             return g;
         }
 
+        public bool matchThese()
+        {
+            // prepare - we know we have left, maybe up.
+            List<Image> remains = new List<Image>();
+            foreach(var t in vertices)
+            {
+                if (finalleft == null)
+                {
+                    if (finalup == null)
+                        remains.Add(t);
+                    else
+                    if (t.num != finalup.num)
+                        remains.Add(t);
+                }
+                else if (t.num != finalleft.num)
+                {
+                    if (finalup == null)
+                        remains.Add(t);
+                    else 
+                    if (t.num != finalup.num)
+                        remains.Add(t);
+                }
+            }
+
+            if (remains.Count == 0) // we're bottom right do nowt.
+                return true;
+
+            if (remains.Count == 1)
+            {
+                if (finalup == null && finalleft != null) // we're right - it's down.
+                {
+                    finalright = null;
+                    finaldown = remains[0];
+                    finaldown.finalup = this;
+                    return true;
+                }
+
+                if (finalup != null && finalleft == null) // we're bottom left
+                {
+                    finalright = remains[0];
+                    finalright.finalleft = this;
+                    finaldown = null;
+                    return true;
+                }
+
+                if (finalup != null && finalleft != null) // we're bottom middle or right middle
+                {
+                    // we don't rotate so we can just look at the matches.
+                    Image one = remains[0];
+                    if (RotateToFit(right, one, x => x.left))
+                    {
+                        finalright = one;
+                        one.finalleft = this;
+                        finaldown = null;
+                    }
+                    else if (RotateToFit(bottom, one, x => x.top))
+                    {
+                        finaldown = one;
+                        one.finalup = this;
+                        finalright = null;
+                    }
+                    return true;
+                }
+
+                return false;
+            }
+
+            // should have two left.
+            if (RotateToFit(right, remains[0], x => x.left) && RotateToFit(bottom, remains[1], x=> x.top))
+            {
+                finalright = remains[0];
+                finaldown = remains[1];
+                return true;
+            }
+            else if (RotateToFit(right, remains[1], x => x.left) && RotateToFit(bottom, remains[0], x => x.top))
+            {
+                finalright = remains[1];
+                finaldown = remains[0];
+                return true;
+            }
+            return false;
+        }
+
+        private bool RotateToFit(string side, Image im, Func<Image,string> selector)
+        {
+            for(int flip = 0; flip < 4; flip++)
+            {
+                switch (flip)
+                {
+                    case 0: // no flip.
+                        for(int i = 0; i < 4; i++)
+                        {
+                            im.Rotate();
+                            if (side == selector(im))
+                                return true;
+                        }
+                        break;
+                    case 1: // vertflip
+                        im.FlipVert();
+                        for (int i = 0; i < 4; i++)
+                        {
+                            im.Rotate();
+                            if (side == selector(im))
+                                return true;
+                        }
+                        im.FlipVert(); // put it back.
+                        break;
+
+                    case 2: // horizontal flip
+                        im.FlipHori();
+                        for (int i = 0; i < 4; i++)
+                        {
+                            im.Rotate();
+                            if (side == selector(im))
+                                return true;
+                        }
+                        im.FlipHori(); // put it back.
+                        break;
+                    case 3: // horizontal flip
+                        im.FlipHori();
+                        im.FlipVert();
+                        for (int i = 0; i < 4; i++)
+                        {
+                            im.Rotate();
+                            if (side == selector(im))
+                                return true;
+                        }
+                        im.FlipVert();
+                        im.FlipHori(); // put it back.
+                        break;
+                }
+            }
+            return false;
+        }
+
         public string reverseString(string s) {
             string rev = "";
             for(int i = s.Length-1; i>=0; i--)
@@ -116,10 +283,10 @@ namespace _20 {
         bool horizflip = false;
         bool vertiflip = false;
 
-        List<Image> leftvert = null;
-        List<Image> rightvert = null;
-        List<Image> upvert = null;
-        List<Image> downvert = null;
+        public List<Image> leftvert = null;
+        public List<Image> rightvert = null;
+        public List<Image> upvert = null;
+        public List<Image> downvert = null;
         List<Image> fleftvert = null;
         List<Image> frightvert = null;
         List<Image> fupvert = null;
@@ -135,13 +302,13 @@ namespace _20 {
             fupvert = new List<Image>();
             fdownvert = new List<Image>();
 
-            Console.WriteLine($"Vertex {num} has {NumVertices} vertices.");
-            Console.ReadLine();
+            //Console.WriteLine($"Vertex {num} has {NumVertices} vertices.");
+            //Console.ReadLine();
             foreach(var im in vertices) {
 
                 byte b = matchImage(im);
                 string s = matchImageCondition(im);
-                Console.WriteLine($"{im.num} :: {s}");
+                //Console.WriteLine($"{im.num} :: {s}");
                 if ((b & Tmask) > 0) {
                     upvert.Add(im);
                 }
@@ -169,6 +336,7 @@ namespace _20 {
                 
             }
 
+            /*
             LinesAndLines(upvert,"up vertices");
             LinesAndLines(downvert,"down vertices");
             LinesAndLines(leftvert, "left vertices");
@@ -177,6 +345,7 @@ namespace _20 {
             LinesAndLines(fdownvert,"downf vertices");
             LinesAndLines(fleftvert, "leftf vertices");
             LinesAndLines(frightvert, "rightf vertices");
+            */
         }
 
         private void LinesAndLines(List<Image> verts, string verttext) {
@@ -185,6 +354,59 @@ namespace _20 {
                 foreach(var im in verts) Console.Write($" {im.num}");
                 Console.WriteLine();
             }
+        }
+
+        public void FlipVert()
+        {
+            int len = image[0].Length;
+            for(int i = 0; i < len; i++)
+            {
+                image[i] = reverseString(image[i]);
+            }
+            CalculateSides(len);
+        }
+
+        public void FlipHori()
+        {
+            int len = image[0].Length;
+            string[] newimage = new string[len];
+            for (int i = 0; i < len; i++)
+            {
+                newimage[(len-i) - 1] = image[i];
+            }
+            image = newimage;
+            CalculateSides(len);
+
+        }
+
+        public void Rotate()
+        {
+            int len = image[0].Length;
+            string[] newimg = new string[len]; // assumes square.
+            for (int i = 0; i < len; i++)
+            {
+                string line = "";
+                for (int j = 0; j < len; j++)
+                {
+                    line += image[j][i];
+                }
+                newimg[i] = line;
+            }
+
+            CalculateSides(len);
+        }
+
+        private void CalculateSides(int len)
+        {
+            top = image[0];
+            bottom = image[len - 1];
+            left = findEdge(x => x[0]);
+            right = findEdge(x => x[x.Length - 1]);
+
+            reversetop = reverseString(top);
+            reversebottom = reverseString(bottom);
+            reverseleft = reverseString(left);
+            reverseright = reverseString(right);
         }
     }
 }
--- 20/Program.cs
+++ 20/Program.cs
@@ -14,22 +14,24 @@ namespace _20
             var f = File.OpenRead(args[0]);
             StreamReader sr = new StreamReader(f);
 
-            SortedDictionary<int,Image> images = new SortedDictionary<int, Image>();
+            SortedDictionary<int, Image> images = new SortedDictionary<int, Image>();
             line = sr.ReadLine();
-            while(line != null) {
+            while (line != null)
+            {
                 // Tile <num>:
-                var numstr = line.Trim().Substring(5,line.Trim().Length-6);
+                var numstr = line.Trim().Substring(5, line.Trim().Length - 6);
                 int num = Int32.Parse(numstr);
                 WriteLine($"Getting image {num}");
                 // ten lines:
                 string[] image = new string[10];
-                for(int i = 0; i < 10; i++) {
+                for (int i = 0; i < 10; i++)
+                {
                     line = sr.ReadLine().Trim();
                     image[i] = line;
                 }
 
                 Image im = new Image(image, num);
-                images.Add(num,im);
+                images.Add(num, im);
 
                 // blank
                 line = sr.ReadLine();
@@ -37,11 +39,15 @@ namespace _20
             }
 
             Console.WriteLine($"Found {images.Count} images.");
-            foreach(var k1 in images.Keys) {
-                foreach(var k2 in images.Keys) {
-                    if (k2 != k1) {
+            foreach (var k1 in images.Keys)
+            {
+                foreach (var k2 in images.Keys)
+                {
+                    if (k2 != k1)
+                    {
                         byte n = images[k1].matchImage(images[k2]);
-                        if (n > 0) {
+                        if (n > 0)
+                        {
                             WriteLine($"{k1} matches {k2} so adding {k1} as vertes to {k2} and {k2} as a vertex to {k1}");
                             images[k1].AddVertex(images[k2]);
                             images[k2].AddVertex(images[k1]);
@@ -53,8 +59,10 @@ namespace _20
             long sum = 1;
             Image topleft = null;
 
-            foreach(var (n,k) in images) {
-                if (k.NumVertices <= 2) { // ToDo
+            foreach (var (n, k) in images)
+            {
+                if (k.NumVertices <= 2)
+                { // ToDo
                     sum = sum * k.num;
                     WriteLine($"One corner is {k}");
                 }
@@ -63,9 +71,63 @@ namespace _20
             // for test expect 20899048083289 got 20899048083289
             WriteLine($"sum is {sum}");
 
-            foreach(var (n,k) in images) {
-                Console.WriteLine($"Orientating {n}");
-                k.Orientate();
+            // Part2.
+
+            List<Image> corners = new List<Image>();
+            foreach (var (n, k) in images)
+            {
+                if (k.NumVertices == 2)
+                {
+                    corners.Add(k);
+                }
+            }
+
+            foreach (var k in corners)
+            {
+                WriteLine($"{k.num} is a corner.");
+                if (k.TopLeft())
+                {
+                    topleft = k;
+                    WriteLine($"{k.num} is now top left!");
+                    break;
+                }
+            }
+
+            if (topleft == null)
+            {
+                WriteLine("No top left found");
+                return;
+            }
+
+            Stack<Image> remaining = new Stack<Image>();
+
+            remaining.Push(topleft.finalright);
+            remaining.Push(topleft.finaldown);
+
+            while(remaining.Count > 0)
+            {
+                var img = remaining.Pop();
+                if(img.matchThese())
+                {
+                    if (img.finalright != null) remaining.Push(img.finalright);
+                    if (img.finaldown != null) remaining.Push(img.finaldown);
+                }
+            }
+
+            Image bot;
+            var imgg = topleft;
+            int size = (int) Math.Sqrt(images.Count);
+            for (int i = 0; i < (size+1); i++)
+            {
+                bot = imgg.finaldown;
+                for (int j = 0; j < (size+1); j++)
+                {
+                    WriteLine($"({i},{j}) is {imgg.num}");
+                    if (imgg.finalright == null)
+                        break;
+                    imgg = imgg.finalright;
+                }
+                imgg = bot;
             }
         }
     }
