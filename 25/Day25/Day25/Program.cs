c8e5bd5c3b734f7c4562d827714182b4e8bcd304
Andrew Douglas <potterboy67@outlook.com>
2020-12-25 10:58:55 +0000

    all 50 stars achieved whoop whoop!

---
+++ 25/Day25/Day25/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Day25
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            //long doorpkey = 17807724;
+            //long cardpkey = 5764801;
+
+            long doorpkey = 13316116;
+            long cardpkey = 13651422;
+
+            long subjectnum = 7;
+
+            long divv = 20201227;
+
+            long value = 1;
+            long adder = 7;
+
+            int c = 0;
+            long rem;
+
+            while (true)
+            {
+                value = (value * subjectnum);
+                Math.DivRem(value, divv, out rem);
+                //Console.WriteLine($"value={value}, divv={divv}, rem={rem}");
+                value = rem;
+
+                if (value == doorpkey)
+                {
+                    Console.WriteLine($"loop value is {c}");
+                    break;
+                }
+                c += 1;
+            }
+
+            value = 1;
+            subjectnum = cardpkey;
+
+            for (int i = 0; i <= c; i++)
+            {
+                value = (value * subjectnum);
+                Math.DivRem(value, divv, out rem);
+                //Console.WriteLine($"value={value}, divv={divv}, rem={rem}");
+                value = rem;
+            }
+
+            Console.WriteLine($"Final key is {value}");
+
+        }
+    }
+}
